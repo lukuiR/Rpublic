20e1d7ceefe693947a63561897316062360d380d
lukuiR <35996054+lukuiR@users.noreply.github.com>
2018-04-20 08:23:48 +0200

    update

--- erum_jr/server.R
+++ erum_jr/server.R
@@ -1,130 +1,123 @@
-
-
-#####
-#server
-######################
-
+#server.r start
 # server.R
-
+# read needed libraries
 library(quantmod)
 library(ggplot2)
 library(plotly)
 library(DT)
-
 library(RColorBrewer)
-#read cvs
-aa2=read.csv("cc.csv", header = TRUE, sep=",")
-
+#read csv with continent and country list, needed for filter update
+cc2=read.csv("cc.csv", header = TRUE, sep=",")
+cc2$country<- gsub("\\s", "", cc2$country)
 shinyServer(function(input, output, session) {
   set.seed(122)
   
   ##
-  
+ # country filter update, when we select som continent, in county list will be contries related only to that continent 
   observeEvent(input$cont, {
     cont <- input$cont
     
     # Available model options based on cont
-    choices <- aa2[aa2$continent %in% cont,3]
-    
-    # Update the kraj input object to only display wanted choices                  
+    choices <- cc2[cc2$continent %in% cont,3]
+     # Update the kraj input object to only display wanted choices                  
     updateSelectInput(session, "kraj", choices = choices)
   })
+  ##############
+ # start data related to R Conference data
   
+  conf_csv=read.csv("conf.csv", header = TRUE, sep=",",colClasses=c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+ #create data that is updated base on country filter, I also add conditions that when, user do not select any country, or any continent, we show all available data. In conference it is also filtered by selected year.  
+   conf <- reactive({conf_csv[conf_csv$year %in%  input$yearr & gsub("\\s", "", 
+                                    conf_csv$id) %in% if(is.null(input$kraj)){if(is.null(input$cont)){cc2$country} else (cc2[cc2$continent %in% input$cont,3])} else (input$kraj),]     })
+#render map
   
-  
-  ##########
-  
-  ######graph
-  output$zzz1<- renderText("zzz")
- ###map
-  
-  cff1=read.csv("girl.csv", header = TRUE, sep=",",colClasses=c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
-
-  output$map <- renderLeaflet(
-    leaflet(cff1) %>% addTiles() %>%
-      addMarkers(lng=cff1$lon, lat=cff1$lat#, clusterOptions = markerClusterOptions(maxClusterRadius=20)  
-                 ,label = cff1$miasto ,popup=paste0('<BIG>',cff1$nazwa,'</BIG><br/>Members: ',cff1$me,'<br/>Organizes: ',cff1$orgn,"<br/><img src = ", cff1$logo, " Width = 150><br/> <SMALL>source: wikipedia</SMALL> ")
+  output$map1 <- renderLeaflet(
+    leaflet(conf()) %>% addTiles() %>%
+      addMarkers(lng= conf()$lon, lat= conf()$lat  ,label = conf()$city ,popup=paste0('<BIG>', conf()$nazwa,'</BIG><br/>Web: ', conf()$web,'<br/>Date: ', conf()$date,'<br/>Year: ', conf()$year,"<br/><img src = ", conf()$wiki, " Width = 150><br/> <SMALL>source: wikipedia</SMALL> ")
       ))
-  
-
-  
-  ######graph
-  output$zzz1<- renderText(zzz <- as.character( paste(cff1[cff1$lon==input$map_marker_click$lng,'lon'], input$map_marker_click$lng))
-                           
-                           )
-  output$image <- renderUI({
-    tags$img(height = "200px", src = as.character( paste(cff1$wiki[cff1$lon==input$map_marker_click$lng])))
-  })
-  
-
-  output$zzz2 <- renderDataTable(cff1[cff1$lon==input$map_marker_click$lng,], 
+ #render table
+  output$zzz223 <- renderDataTable(conf()[conf()$lon==input$map1_marker_click$lng,], 
                                   options = list(scrollX = TRUE, autoWidth = TRUE))
-###############2
-  cff2=read.csv("user.csv", header = TRUE, sep=",",colClasses=c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
-  cff2$lon[is.na(cff2$lon)] <- 0
-  cff2$lat[is.na(cff2$lat)] <- 0
   
-  cff12 <- reactive({cff2[ gsub("\\s", "", cff2$kraj) %in% if(is.null(input$kraj)){if(is.null(input$cont)){aa2$country} else (aa2[aa2$continent %in% input$cont,3])} else (input$kraj),] 
-    
+  #render text information based on selected pin on map
+  output$zz13<- renderText(paste(conf()$kraj[conf()$lon==input$map1_marker_click$lng]))
+  output$zz23<- renderText(paste(conf()$miasto[conf()$lon==input$map1_marker_click$lng]))
+  output$zz33<- renderText(paste(conf()$year[conf()$lon==input$map1_marker_click$lng]))
+  output$zz43<- renderText(paste(conf()$date[conf()$lon==input$map1_marker_click$lng]))
+  output$zz53<- renderText(paste(conf()$nazwa[conf()$lon==input$map1_marker_click$lng]))
+  ######graph
+ #render image of location based on selected pin on map 
+  output$image1 <- renderUI({
+    tags$img(height = "300px", src = as.character( paste(conf()$wiki[conf()$lon==input$map1_marker_click$lng])))
+  })
+   #render table based on selected pin on map
+  output$zzz223 <- renderDataTable(conf()[conf()$lon==input$map1_marker_click$lng,], 
+                                  options = list(scrollX = TRUE, autoWidth = TRUE))
+#end of R Conference tab data
+ #######start data related to useR tab
+#read csv file with useR data  
+user_csv=read.csv("user.csv", header = TRUE, sep=",",colClasses=c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+  user_csv$lon[is.na(user_csv$lon)] <- 0
+  user_csv$lat[is.na(user_csv$lat)] <- 0
+#create data that is updated base on country filter, I also add conditions that when, user do not select any country, or any continent, we show all available data    
+  user <- reactive({user_csv[ gsub("\\s", "", user_csv$kraj) %in% if(is.null(input$kraj)){if(is.null(input$cont)){cc2$country} else (cc2[cc2$continent %in% input$cont,3])} else (input$kraj),]    
   }) 
-  
+  # render map
   output$map2 <- renderLeaflet(
-    leaflet(cff12()) %>% addTiles() %>%
-      addMarkers(lng=cff12()$lon, lat=cff12()$lat#, clusterOptions = markerClusterOptions(maxClusterRadius=20)  
-                 ,label = cff12()$miasto ,popup=paste0('<BIG>',cff12()$nazwa,'</BIG><br/>Members: ',cff12()$members,'<br/>Organizes: ',cff12()$organizer,"<br/><img src = ", cff12()$logo, " Width = 150><br/> <SMALL>source: wikipedia</SMALL> ")
+    leaflet(user()) %>% addTiles() %>%
+      addMarkers(lng= user()$lon, lat= user()$lat#, clusterOptions = markerClusterOptions(maxClusterRadius=20)  
+                 ,label = user()$miasto ,popup=paste0('<BIG>', user()$nazwa,'</BIG><br/>Members: ', user()$members,'<br/>Organizes: ', user()$organizer,"<br/><img src = ", user()$logo, " Width = 150><br/> <SMALL>source: wikipedia</SMALL> ")
       ))
-  output$zzz11<- renderText(as.character( paste( input$kraj,is.null(input$kraj),  aa2[aa2$continent %in% input$cont,3], cff12()[cff12()$lon==input$map2_marker_click$lng,'lon'], input$map2_marker_click$lng
-                                                )))
+
+  #render text information based on selected pin on map
+  output$zz11<- renderText(paste(user()$kraj[user()$lon==input$map2_marker_click$lng]))
+  output$zz21<- renderText(paste(user()$miasto[user()$lon==input$map2_marker_click$lng]))
+  output$zz31<- renderText(paste(user()$me[user()$lon==input$map2_marker_click$lng]))
+  output$zz41<- renderText(paste(user()$org[user()$lon==input$map2_marker_click$lng]))
+  output$zz51<- renderText(paste(user()$nazwa[user()$lon==input$map2_marker_click$lng]))
+  #render image of location based on selected pin on map
+  output$image2 <- renderUI({
+    tags$img(height = "300px", src = as.character( paste(cff12()$wiki[cff12()$lon==input$map2_marker_click$lng])))
+  })
+   #render image of group logo based on selected pin on map
+  output$image22 <- renderUI({
+    tags$img(height = "200px", src = as.character( paste(cff12()$logo[cff12()$lon==input$map2_marker_click$lng])))
+  })
+  #render table
   output$zzz22 <- renderDataTable(cff12()[cff12()$lon==input$map2_marker_click$lng ,], 
-                                 options = list(scrollX = TRUE, autoWidth = TRUE))
-  
-  
-#########2  
-  
-  ###############3
-  
-  cff123=read.csv("conf.csv", header = TRUE, sep=",",colClasses=c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+                                 options = list(scrollX = TRUE, autoWidth = TRUE))  
+#########end of data related to useR tab  
 
-  
-  dataInput <- reactive({cff123[cff123$year %in%  input$yearr & gsub("\\s", "", 
-                                    cff123$id) %in% if(is.null(input$kraj)){if(is.null(input$cont)){aa2$country} else (aa2[aa2$continent %in% input$cont,3])} else (input$kraj),] 
-  
-    })
-
-  
-  output$map23 <- renderLeaflet(
-    leaflet(dataInput()) %>% addTiles() %>%
-      addMarkers(lng=dataInput()$lon, lat=dataInput()$lat#, clusterOptions = markerClusterOptions(maxClusterRadius=20)  
-                 ,label = dataInput()$city ,popup=paste0('<BIG>',dataInput()$nazwa,'</BIG><br/>Web: ',dataInput()$web,'<br/>Date: ',dataInput()$date,'<br/>Year: ',dataInput()$year,"<br/><img src = ", dataInput()$wiki, " Width = 150><br/> <SMALL>source: wikipedia</SMALL> ")
+#start data related to tab R Girls
+ #read R girls csv data 
+  girl_csv=read.csv("girl.csv", header = TRUE, sep=",",colClasses=c("NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+#create data that is updated base on country filter, I also add conditions that when, user do not select any country, or any continent, we show all available data  
+girl <- reactive({girl_csv[ gsub("\\s", "", girl_csv$kraj) %in% if(is.null(input$kraj)){if(is.null(input$cont)){cc2$country} else (cc2[cc2$continent %in% input$cont,3])} else (input$kraj),] 
+}) 
+# render map in leaflet based on girl.csv file  
+  output$map3 <- renderLeaflet(
+    leaflet(girl()) %>% addTiles() %>%
+      addMarkers(lng= girl()$lon, lat= girl()$lat#, clusterOptions = markerClusterOptions(maxClusterRadius=20)  
+                 ,label = girl ()$miasto ,popup=paste0('<BIG>', girl()$nazwa,'</BIG><br/>Members: ', girl()$me,'<br/>Organizes: ', girl()$orgn,"<br/><img src = ", girl()$logo, " Width = 150><br/> <SMALL>source: wikipedia</SMALL> ")
       ))
-  
-  output$zzz223 <- renderDataTable(dataInput()[dataInput()$lon==input$map23_marker_click$lng,], 
+  #render text information based on selected pin on map
+  output$zz1<- renderText(paste(girl()$kraj[girl()$lon==input$map3_marker_click$lng]))
+  output$zz2<- renderText(paste(girl()$miasto[girl()$lon==input$map3_marker_click$lng]))
+  output$zz3<- renderText(paste(girl()$me[girl()$lon==input$map3_marker_click$lng]))
+  output$zz4<- renderText(paste(girl()$orgn[girl()$lon==input$map3_marker_click$lng]))
+  output$zz5<- renderText(paste(girl()$nazwa[girl()$lon==input$map3_marker_click$lng]))
+  #render image of location based on selected pin on map
+  output$image3 <- renderUI({
+    tags$img(height = "300px", src = as.character( paste(girl()$wiki[girl()$lon==input$map3_marker_click$lng])))
+  })
+    #render image of group logo based on selected pin on map
+  output$image33 <- renderUI({
+    tags$img(height = "200px", src = as.character( paste(girl()$logo[girl()$lon==input$map3_marker_click$lng])))
+  })
+#render table
+  output$table3 <- renderDataTable(girl()[girl()$lon==input$map3_marker_click$lng,], 
                                   options = list(scrollX = TRUE, autoWidth = TRUE))
-  
-  
-  #########3
- url1<- reactive({"webshot.png"
-    webshot(as.character( paste(y[y$lon==input$map_marker_click$lng,'wiki'])), delay = 0.5)
-    if (is.null(input$map_marker_click$lng)){NULL}
-    else("webshot.png")
-  })
-
-  
-  output$image2 <- renderImage({
-    try(list(
-        src = url1(),
-        contentType = "image/png",
-        
-        width = 400,
-        alt = "Face"
-      ),options(show.error.messages = FALSE))
-    
-  }, deleteFile = FALSE)
-  
-  #######test
-  output$selected_var <- renderText({ 
-    paste(input$yearr,",")
-  })
-
+#end of data related to R Girls
   
 })
+#server.r end
