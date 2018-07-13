c23a3cfdd1d2a54697773b2e3509dff6472c386b
lukuiR <35996054+lukuiR@users.noreply.github.com>
2018-07-13 11:46:21 +0200

    Update CountDiv.py

--- python codi/CountDiv.py
+++ python codi/CountDiv.py
@@ -1,1 +1,9 @@
+#https://app.codility.com/demo/results/trainingDVZWUV-ETN/
 
+
+def solution(A, B, K):
+    # write your code in Python 3.6
+    if A%K==0:
+        return (B-A)//K+1
+    else:
+        return (B-A+A%K)//K
