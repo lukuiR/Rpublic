df81f406a12df5e8837d0059229a71bdfb7909c8
lukuiR <35996054+lukuiR@users.noreply.github.com>
2018-07-13 10:37:35 +0200

    Update FrogJmp.py

--- python codi/FrogJmp.py
+++ python codi/FrogJmp.py
@@ -1,1 +1,11 @@
 #https://app.codility.com/demo/results/trainingVCZ9H8-883/
+
+def solution(X, Y, D):
+    # write your code in Python 3.6
+    z=Y-X
+    if Y < X or D <= 0:
+        raise Exception("Invalid arguments")
+    if z%D==0:
+        return z//D
+    else:
+        return z//D+1
