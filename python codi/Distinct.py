c27ae73f67f3340540b448a7f89429e08b1be0ff
lukuiR <35996054+lukuiR@users.noreply.github.com>
2018-07-13 13:33:08 +0200

    Update Distinct.py

--- python codi/Distinct.py
+++ python codi/Distinct.py
@@ -1,1 +1,15 @@
+#https://app.codility.com/demo/results/training76QMJ9-7RE/
 
+def solution(A):
+    if len(A) == 0:
+        return 0
+ 
+    A.sort()
+ 
+    nr_values = 1
+ 
+    for i in range(1, len(A)):
+        if A[i] != A[i-1]:
+            nr_values += 1
+ 
+    return nr_values
