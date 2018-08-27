cb7932cea31896b7c15af461be99f60933137c01
lukuiR <35996054+lukuiR@users.noreply.github.com>
2018-08-27 13:28:10 +0200

    Update project_m.R

--- algorytmy/project_m.R
+++ algorytmy/project_m.R
@@ -25,3 +25,50 @@ ag_dat <- group_by(dat, Employment__1 , `Career Level` ,`Chief/Not` , `Company N
   summarise(
     n = n()
   )
+
+#spread
+
+#BC
+ag_dat1 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, Employment__1 )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = Employment__1, value = n) #, fill =0)
+
+ag_dat2 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Chief/Not` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Chief/Not`, value = n) #, fill =0)
+
+ag_dat3 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Career Level` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Career Level`, value = n) #, fill =0)
+
+ag_dat4 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, Gender )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = Gender, value = n) #, fill =0)
+
+ag_dat5 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Job Family` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Job Family`, value = n) #, fill =0)
+
+ag_dat6 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Year of retirement` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Year of retirement`, value = n) #, fill =0)
+
+ag_dat66=ag_dat6[,c(1:5,17:22)]
+
+zz=merge(ag_dat1,ag_dat2)
+zz=merge(zz,ag_dat3)
+zz=merge(zz,ag_dat4)
+zz=merge(zz,ag_dat5)
+zz=merge(zz,ag_dat66)
