287412d907e09bd4d06135f526367c870f9769fe
lukuiR <35996054+lukuiR@users.noreply.github.com>
2018-09-16 21:10:42 +0200

    Update app2.R

--- ercel/app2.R
+++ ercel/app2.R
@@ -14,14 +14,103 @@ library(data.table)
 # needed to display the name of the airline but pass its
 # Carrier code as the value
 
-
+{
 #select only required columns
 datraw1 <- read_excel("C:/Users/Luke/Documents/r_datamining/data/HRIS data with subfamilies 20180823.xlsx",col_names = TRUE,skip=1)
 
-dat1 <- select(datraw1, Gender, Employment__1 , `Career Level` ,`Chief/Not` , `Company Name`, `SBU/MFG/CF`, Subfamily , Family , RegionCode, `Level 1`, `Year of retirement`, `Job Family`, `Encrypted Ids`)
+dat <- select(datraw1, Gender, Employment__1 , `Career Level` ,`Chief/Not` , `Company Name`, `SBU/MFG/CF`, Subfamily , Family , RegionCode, `Level 1`, `Year of retirement`, `Job Family`, `Encrypted Ids`)
 
-fam <- unique(dat1$Family)
-reg <- unique(dat1$RegionCode)
+fam <- unique(dat$Family)
+reg <- unique(dat$RegionCode)
+
+dat <- mutate(dat, 
+              `SBU/MFG/CF Name`= case_when(`SBU/MFG/CF` == "SBU" ~ `Level 1`,  `SBU/MFG/CF` == "MFG Sites" ~ `Company Name`, TRUE ~ `SBU/MFG/CF`)
+)
+
+#Remove not needed job Family
+dat$`Job Family`[!(dat$`Job Family` %in% c('Professionals',	'Technicians', 'Executives',	'Advisor & Consultant',	'Managers',	'Supervisor',	
+                                           'Administrators',	'Operators',	'Superintendent & Section Head',	
+                                           'Security & Safety', 'Para Professional'))] <- 'Other'
+
+#Aggregate, count id
+ag_dat <- group_by(dat, Employment__1 , `Career Level` ,`Chief/Not` , `Company Name`, `SBU/MFG/CF`, Subfamily , Family , RegionCode, `Level 1`, `Year of retirement`, `Job Family`, `SBU/MFG/CF Name`) %>% 
+  summarise(
+    n = n()
+  )
+
+#spread
+
+#BC
+ag_dat1 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, Employment__1 )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = Employment__1, value = n) #, fill =0)
+
+ag_dat2 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Chief/Not` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Chief/Not`, value = n) #, fill =0)
+
+colnames(ag_dat2)[dim(ag_dat2)[2]] <- "`Chief/Not NA`"
+
+ag_dat3 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Career Level` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Career Level`, value = n) #, fill =0)
+
+colnames(ag_dat3)[dim(ag_dat3)[2]] <- "`Career Level NA`"
+
+ag_dat4 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, Gender )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = Gender, value = n) #, fill =0)
+
+colnames(ag_dat4)[dim(ag_dat4)[2]] <- "`Gender NA`"
+
+ag_dat5 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Job Family` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Job Family`, value = n) #, fill =0)
+
+ag_dat6 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, `Year of retirement` )%>% 
+  summarise(
+    n = n()
+  ) %>%
+  spread(key = `Year of retirement`, value = n) #, fill =0)
+
+#remove not needed years of retirement
+#ag_dat66=ag_dat6[, c('Subfamily' , 'Family' ,'SBU/MFG/CF', 'SBU/MFG/CF Name', 'RegionCode', '2018', '2019', '2020','2021','2022', '2023')] #1:5,17:22)]
+
+ag_dat66 <- select( ag_dat6, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode, '2018',`2019`,`2020`,`2021`,`2022`,`2023`)
+
+ag_dat6 <- group_by(dat, Subfamily , Family ,`SBU/MFG/CF`, `SBU/MFG/CF Name`, RegionCode )%>% 
+  summarise(
+    n = n()
+  )
+
+db <- ag_dat1 %>% 
+  inner_join(ag_dat2)%>% 
+  inner_join(ag_dat3)%>% 
+  inner_join(ag_dat4)%>% 
+  inner_join(ag_dat5)%>% 
+  inner_join(ag_dat66)%>% 
+  inner_join(ag_dat6)
+db <- mutate(db,  Manual = n)
+
+db[["Modify"]]<-
+  paste0('
+         <div class="btn-group" role="group" aria-label="Basic example">
+         <button type="button" class="btn btn-secondary delete" id=modify_',1:nrow(db),'>Change</button>
+         </div>
+         
+         ')
+
+}
 
 ui <- dashboardPage(skin = "green",
                     dashboardHeader(title = "Mercel" #,titleWidth = 200
@@ -390,17 +479,12 @@ server <- function(input, output, session) {
   
   
   ################################################################3change
-  tow=reactiveValues()
-  tow$Data=data.table(
-    dat1
-    
-  )
-  #####
+
   vals=reactiveValues()
-  vals$Data=data.table(dat1
+  vals$Data=data.table(db
   )
   
-  newEntry <- observe({vals$Data <- data.table(dat1[dat1$RegionCode %in%  if(is.null(input$rg)){reg} else (input$rg),]
+  newEntry <- observe({vals$Data <- data.table(db[db$RegionCode %in%  if(is.null(input$rg)){reg} else (input$rg),]
       )
   })
   
@@ -483,9 +567,9 @@ server <- function(input, output, session) {
   ##co modyfikuje
   output$row_modif<-renderDataTable({
     selected_row=as.numeric(gsub("modify_","",input$lastClickkId))
-    old_row=dataset()[selected_row,c('n','Manual')]
+    old_row=vals$Data[selected_row,c('n','Manual')]
     row_change=list()
-    row_change[[1]]=dataset()[selected_row,c('n')]
+    row_change[[1]]=vals$Data[selected_row,c('n')]
     row_change[[2]]=paste0('<input class="new_input" type="number" id=new_',2,'><br>')
     row_change=as.data.table(row_change)
     setnames(row_change,colnames(old_row))
@@ -507,7 +591,9 @@ server <- function(input, output, session) {
                    }
                  })
                  DF=data.frame(lapply(newValue, function(x) t(data.frame(x))))
-                 dataset()[as.numeric(gsub("modify_","",input$lastClickkId)),'Manual']<-DF
+                 #dataset()[as.numeric(gsub("modify_","",input$lastClickkId)),'Manual']<-DF
+                 
+                 vals$Data[as.numeric(gsub("modify_","",input$lastClickkId)),'Manual']<-DF
                }
   )
   ######
