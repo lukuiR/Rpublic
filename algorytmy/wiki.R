fbf0e2fb4e267d1e6552d81fff0d3c826d43e21e
lukuiR <35996054+lukuiR@users.noreply.github.com>
2019-02-01 15:28:39 +0100

    Update wiki.R

--- algorytmy/wiki.R
+++ algorytmy/wiki.R
@@ -1,5 +1,7 @@
 #https://github.com/mkearney/wiki_data
 
+library('data.table')
+
 untar('https://dumps.wikimedia.org/other/pageviews/2018/2018-12/pageviews-20181231-230000.gz')
 
 
@@ -8,12 +10,18 @@ t<-read.table(gzfile(fread(paste("https://dumps.wikimedia.org/other/pageviews/20
 
 file_name<-"pageviews-20190124-120000.gz"
 
-t<-read.table(gzfile("pageviews-20190124-120000.gz"),header = TRUE, fill = TRUE)
 
+tcv<-read.table(gzfile("clickstream-plwiki-2018-12.tsv.gz"), fill = TRUE)
+
+te<-read.table(gzfile("clickstream-enwiki-2018-12.tsv.gz"), fill = TRUE)
 
 source("read_wiki_data.R")
 
+pwe<-read.table(gzfile(file_name), fill = TRUE)
+
 x <- wiki_data(file_name, skip = 100, n_max = 100)
+
+unique( pwe$V1[pwe$V1 %like% 'app'])
 
 i=0
 j=0
@@ -31,3 +39,5 @@ max_en=3603001
 min_pl=6003001
 max_pl=6303001
 x <- wiki_data(file_name, skip = j-50000, n_max = 1)
+
+byDayTime[order(-byDayTime$count),][1:100,]
